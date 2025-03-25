9e3e83abf2a0386fcc82406747839bdafa4237cf
dvdt5 <dagtekindavut07@gmail.com>
2025-03-25 22:27:24 +0300

    added buttons for category table

--- School_Project---Q&A_App/DTOs/CategoryDto.cs
+++ School_Project---Q&A_App/DTOs/CategoryDto.cs
@@ -1,7 +1,10 @@
-﻿namespace School_Project___Q_A_App.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace School_Project___Q_A_App.DTOs
 {
     public class CategoryDto : BaseDto
     {
+        [Required(ErrorMessage = "Category Name is required!")]
         public string Name { get; set; }
     }
 }
