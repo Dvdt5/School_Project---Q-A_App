24d4d575565c83c1d4d3120dea23d4940c3b8608
dvdt5 <dagtekindavut07@gmail.com>
2025-04-16 16:57:02 +0300

    added login method

--- School_Project---Q&A_App/Controllers/UserController.cs
+++ School_Project---Q&A_App/Controllers/UserController.cs
@@ -4,15 +4,19 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 using School_Project___Q_A_App.DTOs;
 using School_Project___Q_A_App.Models;
 using School_Project___Q_A_App.Repositories;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace School_Project___Q_A_App.Controllers
 {
     [Route("[controller]")]
     [ApiController]
-    
+    [Authorize(Roles = "Admin")]
     public class UserController : ControllerBase
     {
         private readonly UserManager<AppUser> _userManager;
@@ -92,7 +96,7 @@ namespace School_Project___Q_A_App.Controllers
         }
 
         [HttpPut]
-        public async Task<ResultDto> Update (UserDto userDto)
+        public async Task<ResultDto> Update(UserDto userDto)
         {
             var response = new ResultDto
             {
@@ -111,7 +115,7 @@ namespace School_Project___Q_A_App.Controllers
         }
 
         [HttpDelete]
-        public async Task<ResultDto> Delete (string id)
+        public async Task<ResultDto> Delete(string id)
         {
             var user = await _userManager.FindByIdAsync(id.ToString());
             await _userManager.DeleteAsync(user);
@@ -125,6 +129,78 @@ namespace School_Project___Q_A_App.Controllers
 
         }
 
+        [HttpPost("SignIn")]
+        [AllowAnonymous]
+        public async Task<ResultDto> SignIn(LoginDto dto)
+        {
+            var user = await _userManager.FindByNameAsync(dto.UserName);
+
+            if (user is null)
+            {
+                var response = new ResultDto
+                {
+                    Success = false,
+                    Message = "This User Doesnt Exit!",
+                };
+                return response;
+            }
+            var isPasswordCorrect = await _userManager.CheckPasswordAsync(user, dto.Password);
+
+            if (!isPasswordCorrect)
+            {
+                var response = new ResultDto
+                {
+                    Success = false,
+                    Message = "Username or Password is wrong!",
+                };
+                return response;
+            }
+
+            var userRoles = await _userManager.GetRolesAsync(user);
+
+            var authClaims = new List<Claim>
+            {
+            new Claim(ClaimTypes.Name, user.UserName),
+            new Claim(ClaimTypes.NameIdentifier, user.Id),
+            new Claim("JWTID", Guid.NewGuid().ToString()),
+
+            };
+
+            foreach (var userRole in userRoles)
+            {
+                authClaims.Add(new Claim(ClaimTypes.Role, userRole));
+            }
+
+            var token = GenerateJWT(authClaims);
+
+            var responseDone = new ResultDto
+            {
+                Success = true,
+                Message = "Login Successfull!",
+            };
+            return responseDone;
+        }
+        private string GenerateJWT(List<Claim> claims)
+        {
+
+            var accessTokenExpiration = DateTime.Now.AddMinutes(Convert.ToDouble(_configuration["AccessTokenExpiration"]));
+
+
+            var authSecret = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+
+            var tokenObject = new JwtSecurityToken(
+            issuer: _configuration["Jwt:Issuer"],
+            audience: _configuration["Jwt:Audience"],
+            expires: accessTokenExpiration,
+            claims: claims,
+            signingCredentials: new SigningCredentials(authSecret, SecurityAlgorithms.HmacSha256)
+            );
+
+            string token = new JwtSecurityTokenHandler().WriteToken(tokenObject);
+
+            return token;
+        }
+   
 
     }
 }
